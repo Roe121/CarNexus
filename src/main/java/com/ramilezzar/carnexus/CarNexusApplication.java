dba8c2521d6a7730a904c23cf6ea24f781c687df
Roe121 <lezzarrami@gmail.com>
2024-06-02 16:20:10 +0200

    Initial commit

---
+++ src/main/java/com/ramilezzar/carnexus/CarNexusApplication.java
@@ -0,0 +1,13 @@
+package com.ramilezzar.carnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramilezzar/carnexus/CarNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramilezzar.carnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
